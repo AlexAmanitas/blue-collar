49d78868cd1596365c150764f1283e5b3327e1ad
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-07-21 08:23:55 +0300

    easter

---
+++ js/App.tsx
@@ -0,0 +1,32 @@
+// App.tsx
+import React from "react";
+import "./App.css";
+import EasterBreaker from "./EasterBreaker";
+import { RepositoryMetrics } from "repository-metrics";
+import logo from "./profile.png";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} alt="logo" />
+				<p>Break this page! but be careful...</p>
+				<EasterBreaker />
+				<br />
+				<a
+					className="App-link"
+					href="https://github.com/Ivan-Corporation/Easter-break"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Github
+				</a>
+
+				<hr />
+				<RepositoryMetrics owner="Ivan-Corporation" repo="Easter-break" theme="dark" />
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ js/EasterBreaker.tsx
@@ -0,0 +1,12 @@
+//  EasterBreaker.tsx
+import easterBreaker from "easter-break";
+import "./easterBreaker.css";
+
+const EasterBraker = () => {
+	return (
+		<button className="easterBreaker" onClick={easterBreaker}>
+			Break me{" "}
+		</button>
+	);
+};
+export default EasterBraker;
